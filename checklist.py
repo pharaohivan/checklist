793e62bc04a8972cf16ecc1186ced0c85156d615
Ivan <ivanhallcloninger@gmail.com>
2019-09-03 10:00:59 -0700

    final

--- checklist.py
+++ checklist.py
@@ -5,10 +5,6 @@ checklist = list()
 def create(item):
     checklist.append('Blue')
 
-print(checklist)
-checklist.append('Orange')
-print(checklist)
-
 # READ
 def read(index):
     item = checklist[index]
@@ -21,6 +17,27 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print("{} {}".format(index, list_item))
+        index += 1
+
+def mark_completed(index):
+    # Add code here that marks an item as completed
+    checklist[index] = ("√" + item)
+
 def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    #print(read(1))
 
 test()
