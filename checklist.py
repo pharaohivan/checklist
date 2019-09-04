2e0881f1272357dafd640a1efd1aa8c4514f9120
Ivan <ivanhallcloninger@gmail.com>
2019-09-03 21:42:18 -0700

    third commit

--- checklist.py
+++ checklist.py
@@ -1,19 +1,19 @@
+# Create our Checklist
+checklist = []
 
-
-checklist = list()
-
+# CREATE
 def create(item):
-    checklist.append('Blue')
+    checklist.append(item)
 
 # READ
 def read(index):
-    item = checklist[index]
-    return item
+    return checklist[index]
+
 # UPDATE
 def update(index, item):
     checklist[index] = item
 
-    # DESTROY
+# DESTROY
 def destroy(index):
     checklist.pop(index)
 
@@ -21,13 +21,45 @@ def list_all_items():
     index = 0
     for list_item in checklist:
         print("{} {}".format(index, list_item))
-        index += 1
+        index +=1
 
 def mark_completed(index):
     # Add code here that marks an item as completed
-    checklist[index] = ("√" + item)
+    update(index, "√" + checklist[index])
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+
+    # Catch all
+    else:
+        print("Unknown Option")
+
+    return True
+
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
 
 def test():
+    # add testing code
     create("purple sox")
     create("red cloak")
 
@@ -38,6 +70,17 @@ def test():
     destroy(1)
 
     print(read(0))
-    #print(read(1))
+    # print(read(1))
 
-test()
+    list_all_items()
+
+    user_value = user_input("Please Enter a value:")
+    print(user_value)
+
+#test()
+
+running = True
+while running:
+    selection = user_input(
+        "Press C to add to list, R to Read from list and P to display list")
+    running = select(selection)
